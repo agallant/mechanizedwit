d5f632625384602cfc331f0dc12eb0534a6915bf
soycode <soycode@users.noreply.github.com>
2015-04-08 01:39:52 -0700

    refactor HN code, chatbot stub

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Because you're too busy to "socialize"
 --------------------------------------
 
-To install requirements (`easy_install` not `pip` due to [python-graph](https://code.google.com/p/python-graph/)):
+To install requirements:
 
-    easy_install --user `cat requirements.txt`
+   pip install --user -r requirements.txt
 
 Then it's just a matter of `python wit.py` and (after waiting on the [Hacker News API](https://github.com/HackerNews/API) for a bit) you'll get a generated comment based on the discussion in the currently most discussed HN story (out of the top 30).
--- wit.py
+++ wit.py
@@ -22,51 +22,87 @@
 
 from random import sample
 from hackernews import HackerNews
-# TODO - graph idea for deeper model of text
-#from pygraph.classes.graph import graph
+from chatterbot import ChatBot
 
-def mimic(text):
-  mimic_dict = {}
-  for line in text:
-    words = line.split()
-    if len(words) > 1:
-      for i, word in enumerate(words[:-1]):
-        mimic_dict[word] = mimic_dict.get(word, []) + [words[i + 1]]
-      # Use empty string as successor for last word
-      mimic_dict[word[-1]] = mimic_dict.get(word[-1], []) + ['']
-  return mimic_dict
 
-def parrot(text, numchars):
-  mimic_dict = mimic(text)
-  res = ''
-  current_word = sample([line.split()[0] for line in text], 1)[0]
-  numchars -= len(current_word)
-  while numchars > 0:
-    res += current_word
-    current_word = sample(mimic_dict.get(current_word, ['']), 1)[0]
-    if current_word:
-      numchars -= len(current_word)
-    else:
-      numchars = 0  # found no word, stop here
-    if numchars > 0:
-      res += ' '
-      numchars -= 1
-  return res
+class HackerInsight:
+  """Generates Markov-style comments based on HN discussion."""
+  def __init__(self):
+    self.hn = HackerNews()
+    self.stories = []
+    self.comments = []
+    self.mimic_dict = {}
+    self.processed_comments = 0
+
+  def get_new_comments(self):
+    """Pull fresh most-discussed story and get comments from it."""
+    top_stories = [self.hn.item(top_story) for top_story
+                   in self.hn.top_stories()[:30]  # makes frontpage only
+                   if top_story not in [story.id for story in self.stories]]
+    # Currently just grabbing direct descendants, which may be more natural anyway
+    self.stories.append(max(top_stories,
+                            key=lambda story:len(story.kids)
+                            if hasattr(story, 'kids') else 0))
+    comments = [self.hn.item(kid) for kid in self.stories[-1].kids]
+    # Extending means past comments will be remembered/combined - revisit
+    self.comments.extend([comment.text for comment in comments
+                          if hasattr(comment, 'text')])
+
+  def build_mimic_dict(self):
+    """Take a list of strings and return Markov state dict word->word list."""
+    for line in self.comments[self.processed_comments:]:
+      words = line.split()
+      if len(words) > 1:
+        for i, word in enumerate(words[:-1]):
+          self.mimic_dict[word] = self.mimic_dict.get(word, []) + [words[i + 1]]
+        # Use empty string as successor for last word
+        self.mimic_dict[word[-1]] = self.mimic_dict.get(word[-1], []) + ['']
+    self.processed_comments = len(self.comments)
+
+  def generate_comment(self, numchars=117):
+    """Generate a comment from Markov states, default Tweet+link length."""
+    res = ''
+    # Randomly sample first word from all comments first words, then grow
+    current_word = sample([line.split()[0] for line in self.comments], 1)[0]
+    numchars -= len(current_word)
+    while numchars > 0:
+      res += current_word
+      current_word = sample(self.mimic_dict.get(current_word, ['']), 1)[0]
+      if current_word:
+        numchars -= len(current_word)
+      else:
+        numchars = 0  # found no word, stop here
+      if numchars > 0:
+        res += ' '
+        numchars -= 1
+    return res
+
+
+class BackTalker:
+  """Use ChatBot to remember replies and generate new ones."""
+  def __init__(self):
+    self.chatbot = ChatBot()
+    # TODO: decide how to train, e.g. from tweets/CleverBot/etc.
+
 
 def main():
-  hn = HackerNews()
-  top_stories = [hn.item(top_story) for top_story in hn.top_stories()[:30]]
-  # Currently just grabbing direct descendants, which may be more natural anyway
-  most_discussed = max(top_stories,
-                       key=lambda story:len(story.kids)
-                       if hasattr(story, 'kids') else 0)
-  comments = [hn.item(kid) for kid in most_discussed.kids]
-  new_comment = parrot([comment.text for comment in comments
-                        if hasattr(comment, 'text')], 117)
-  # For now, just print the 'clever' comment
-  if hasattr(most_discussed, 'title'):
-    print most_discussed.title
-  print new_comment
+  # For now, just make and run a single time
+  hi = HackerInsight()
+  hi.get_new_comments()
+  hi.build_mimic_dict()
+  # Just print out basic story info and a comment
+  print(hi.stories[-1].title)
+  print(hi.stories[-1].id)
+  print(hi.stories[-1].url)
+  print(hi.generate_comment())
+  # And run another story to make sure it's next-most discussed
+  hi.get_new_comments()
+  hi.build_mimic_dict()
+  print(hi.stories[-1].title)
+  print(hi.stories[-1].id)
+  print(hi.stories[-1].url)
+  print(hi.generate_comment())
+
 
 if __name__ == "__main__":
   main()
